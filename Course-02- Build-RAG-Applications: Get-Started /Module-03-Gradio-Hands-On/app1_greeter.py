cf1e35bf77dfbb39ba52b46f5ad62d4397e0ad71
ArshaR_Unnithan <99828451+arsharunnithan@users.noreply.github.com>
2026-05-21 22:54:12 +0530

    Create a simple Gradio greeter application

---
+++ Course-02- Build-RAG-Applications: Get-Started /Module-03-Gradio-Hands-On/app1_greeter.py
@@ -0,0 +1,13 @@
+import gradio as gr
+
+def greet(name):
+    return "Hello, " + name + "!"
+
+demo = gr.Interface(
+    fn=greet,
+    inputs="text",
+    outputs="text",
+    title="My First Gradio App"
+)
+
+demo.launch()
